8033a589574f818af6d9ae77f9eb5b2d7d75e6ba
kazeyuki1876 <32504095+kazeyuki1876@users.noreply.github.com>
2019-05-11 17:54:44 +0900

    タイトル 落ち花１

---
+++ Assets/kamara.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kamara : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        void Update()
+        {
+           /* if (Input.GetKeyDown(KeyCode.Space))
+                FadeManager.Instance.LoadScene("start", 2.0f);
+                */
+        }
+    }
+}
