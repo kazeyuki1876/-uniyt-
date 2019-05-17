cad607689008b89cbd81ff23774d46dccb69ffb0
kazeyuki1876 <32504095+kazeyuki1876@users.noreply.github.com>
2019-05-17 13:59:30 +0900

    20190517
    11

---
+++ Assets/Script/Start/Start_Camera.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Start_Camera : MonoBehaviour
+{
+
+    public  bool OFFtest;
+
+    
+
+    public bool CameraMetastasis = false;//カメラ転移
+    public bool CameraEnlarge = false;//カメラ拡大
+    
+
+
+    bool i = true;
+
+    public float Acceleration = 0.1f;
+    public void Start()
+    {
+        Camera.main.orthographicSize = 13.7f;
+
+       //OFFtest = GameObject.Find("Titel").GetComponent<Start_TitelTransparent>().test;
+       
+    }
+
+
+    void Update()
+    {
+        OFFtest = false;
+        if (Input.GetKeyDown(KeyCode.J)) {
+            CameraEnlarge = true;
+        }
+        Enlarge();
+
+        if (Acceleration > 0.5f)
+        {
+            Acceleration = 0.5f;
+        }
+       
+            this.transform.position = new Vector3(transform.position.x - Acceleration, transform.position.y, -10.0f);
+
+            Acceleration = Acceleration + Time.deltaTime * 0.02f;
+            if (i)
+            {
+                Debug.Log("OFF");
+                i = false;
+            }
+        
+    }
+    void Enlarge() {
+        if (CameraEnlarge) {
+            if (Camera.main.orthographicSize >= 10.01f) //小于一个放大范围后就不继续放大了 
+            {
+                Camera.main.orthographicSize = Camera.main.orthographicSize - 0.05f;
+            }
+            else
+            {
+                Camera.main.orthographicSize = 10.0f;
+                CameraEnlarge = false;
+            }
+        }
+       
+    }
+
+    void daiyong() {
+        /*if (Input.GetKeyDown(KeyCode.J))
+        {
+            OFF = true;
+        }
+
+        if (OFF2 == true)
+        {
+            if (Camera.main.orthographicSize >= 5.1f) //小于一个放大范围后就不继续放大了 
+            {
+                Camera.main.orthographicSize = Camera.main.orthographicSize - 0.05f;
+            }
+        }
+
+       
+        */
+    }
+}
+
---
+++ Assets/Script/Start/Start_TitelTransparent.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Start_TitelTransparent : MonoBehaviour
+{
+
+
+    //GameObject.Find("脚本所在的物体的名字").GetComponent<脚本名>().函数名();
+    public  bool test = true;
+    public static bool TitelTransparentOff = false; //
+    //Color RGBA
+    float changeRed = 1.0f;
+    float changeGreen = 1.0f;
+    float cahngeBlue = 1.0f;
+    float cahngeAlpha = 1.0f;
+  
+    void TitelTransparent()
+    {
+      
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            TitelTransparentOff = true;
+        }
+        if (TitelTransparentOff) {
+            cahngeAlpha = cahngeAlpha - Time.deltaTime * 0.3f;
+        }
+        if (test == false) { Debug.Log("test" + test); }
+
+
+        // 元の画像の色のまま、半透明になって表示される。
+        this.GetComponent<SpriteRenderer>().color = new Color(changeRed, changeGreen, cahngeBlue, cahngeAlpha);
+    }
+   /* public void test() {
+        Debug.Log("test");
+    }*/
+  
+    
+           
+   
+   
+    
+
+}
