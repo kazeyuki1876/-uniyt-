a23103294006c145d2630ac17ae465672bb04df1
kazeyuki1876 <32504095+kazeyuki1876@users.noreply.github.com>
2019-05-24 12:50:39 +0900

    UI　イラスト　サイズ　調整

---
+++ Assets/Script/Start/Start_UmeNoHanabira.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Start_UmeNoHanabira : MonoBehaviour
+{
+    public bool ImejiMetastasisSwitch = false;
+    public float StartIchi;
+    public float EndiCHI;
+    public float Speed =2.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (ImejiMetastasisSwitch)
+        {
+            if (this.transform.position.x< EndiCHI)//EndiCHI =0;
+            {
+                this.transform.position = new Vector3(transform.position.x + Speed, transform.position.y - Speed, -0.0f);
+            }
+        }
+        // this.transform.position = new Vector3(transform.position.x - Speed, transform.position.y, -10.0f);
+    }
+    void ImejiMetastasisSwitchON()
+    {
+        Debug.Log("test2");
+        ImejiMetastasisSwitch = true;
+    }
+}
